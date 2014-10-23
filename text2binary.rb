0b29c66f80d8c94b9bdcbc68f4649f2d37a124aa
sakas-- <kensakayori@gmail.com>
2014-10-23 16:45:37 +0900

    Update text2binary.rb

--- text2binary.rb
+++ text2binary.rb
@@ -1,5 +1,5 @@
 str = STDIN.gets
 a = str.scan(/.{1,8}/)
 a = a.map { |s| ("0b" + s).to_i(0) }
-ret = a.inject("") { |out, i | out = i.chr + out}  
+ret = a.inject("") { |out, i | out = out + i.chr}  
 print ret
